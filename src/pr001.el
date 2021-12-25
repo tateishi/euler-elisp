c276391f61f48f8c86c1cf97da7a471d80498f54
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-25 16:47:36 +0900

    euler pro001追加

---
+++ src/pr001.el
@@ -0,0 +1,13 @@
+(defun euler:pr001-multiple-of-3-or-5 (n)
+  (or (equal (% n 3) 0) (equal (% n 5) 0)))
+
+(defun euler:pr001-answer (n)
+  (let ((i 0)
+        (x 0))
+    (while (< i n)
+      (if (euler:pr001-multiple-of-3-or-5 i)
+          (setq x (+ x i)))
+      (setq i (1+ i)))
+    x))
+
+(provide 'pr001)
