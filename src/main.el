c481f7f0920576863ce27d93ad4e09b7d067025c
Tateishi Tadatoshi <ishio39@gmail.com>
2021-12-25 17:26:01 +0900

    add comment

--- src/main.el
+++ src/main.el
@@ -1,3 +1,33 @@
+;;; main.el --- The solution for Project euler -*- lexical-binding: t -*-
+
+;; Copyright (C) 2021 TATEISHI Tadatoshi
+
+;; Author: TATEISHI Tadatoshi <ishio39@gmail.com>
+;; Created: 2021/12/25
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Project euler main module.
+
+;;; Code:
+
 (require 'pr001)
 
 (princ (format "PR001: The answer is %d\n" (euler:pr001:answer 10)))
